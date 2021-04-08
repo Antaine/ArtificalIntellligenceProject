e8dc6a0673822da2b27f94e6008bc4460587654d
antaine <aconn@outlook.com>
2021-04-08 17:30:08 +0100

    Safe version 1.0

---
+++ AIProject/src/module-info.java
@@ -0,0 +1,9 @@
+module aIProject {
+	requires javafx.graphics;
+	requires javafx.fxml;
+	requires javafx.controls;
+	requires javafx.base;
+	exports ie.gmit.sw.ai;
+	//opens ie.gmit.sw.ai to javafx.graphics;
+	
+}
